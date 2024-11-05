e7f495ca9127056dad366a21607bc3d9e6abd5b5
Mark Valentino <mike9377mike9377@gmail.com>
2024-11-04 23:09:38 -0500

    memoized lines to improe performance

--- src/components/Crosshair.js
+++ src/components/Crosshair.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 export const Crosshair = ({ children }) => {
   const [dimensions, setDimensions] = useState({ w: 0, h: 0 });
@@ -32,67 +32,78 @@ export const Crosshair = ({ children }) => {
     };
   }, []);
 
+  const leftLineStyle = useMemo(
+    () => ({
+      position: "absolute",
+      width: `calc(${position.x}px - 10px)`,
+      top: position.y,
+      left: 0,
+      height: 1,
+      backgroundColor: "green",
+      zIndex: 9999,
+    }),
+    [position]
+  );
+
+  const rightLineStyle = useMemo(
+    () => ({
+      position: "absolute",
+      width: `calc(${dimensions.w}px - ${position.x}px - 10px)`,
+      top: position.y,
+      left: `calc(${position.x}px + 10px)`,
+      height: 1,
+      backgroundColor: "green",
+      zIndex: 9999,
+    }),
+    [position]
+  );
+
+  const topLineStyle = useMemo(
+    () => ({
+      position: "absolute",
+      width: 1,
+      top: 0,
+      left: position.x,
+      height: `calc(${position.y}px - 10px)`,
+      backgroundColor: "green",
+      zIndex: 9999,
+    }),
+    [position]
+  );
+
+  const bottomLineStyle = useMemo(
+    () => ({
+      position: "absolute",
+      width: 1,
+      bottom: 0,
+      left: position.x,
+      height: `calc(${dimensions.h}px - ${position.y}px - 10px)`,
+      backgroundColor: "green",
+      zIndex: 9999,
+    }),
+    [position]
+  );
+
+  const centerSquareStyle = useMemo(
+    () => ({
+      width: 20,
+      height: 20,
+      position: "absolute",
+      left: position.x - 10,
+      top: position.y - 10,
+      border: "1px solid green",
+      boxSizing: "border-box",
+    }),
+    [position]
+  );
+
   return (
     <>
-      {/* Left line */}
-      <div
-        style={{
-          position: "absolute",
-          width: `calc(${position.x}px - 10px)`,
-          top: position.y,
-          left: 0,
-          height: 1,
-          backgroundColor: "green",
-          zIndex: 9999,
-        }}
-      />
-      {/* Right Line */}
-      <div
-        style={{
-          position: "absolute",
-          width: `calc(${dimensions.w}px - ${position.x}px - 10px)`,
-          top: position.y,
-          left: `calc(${position.x}px + 10px)`,
-          height: 1,
-          backgroundColor: "green",
-          zIndex: 9999,
-        }}
-      />
-      {/* Top Line */}
-      <div
-        style={{
-          position: "absolute",
-          width: 1,
-          top: 0,
-          left: position.x,
-          height: `calc(${position.y}px - 10px)`,
-          backgroundColor: "green",
-          zIndex: 9999,
-        }}
-      />
-      {/* Bottom Line */}
-      <div
-        style={{
-          position: "absolute",
-          width: 1,
-          bottom: 0,
-          left: position.x,
-          height: `calc(${dimensions.h}px - ${position.y}px - 10px)`,
-          backgroundColor: "green",
-          zIndex: 9999,
-        }}
-      />
-      <div
-        style={{
-          width: 20,
-          height: 20,
-          position: "absolute",
-          left: position.x - 10,
-          top: position.y - 10,
-          border: "1px solid green",
-          boxSizing: "border-box"
-        }}
-      />
+      <div style={leftLineStyle} />
+      <div style={rightLineStyle} />
+      <div style={topLineStyle} />
+      <div style={bottomLineStyle} />
+      <div style={centerSquareStyle} />
       <div
         style={{
           position: "fixed",
